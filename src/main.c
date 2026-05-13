d5d2e7758004d52f65b98179220f9de5a39eaaca
chimi <deadxraver@yandex.ru>
2026-05-13 20:12:01 +0300

    line breaks

--- src/main.c
+++ src/main.c
@@ -9,25 +9,30 @@ int main(int argc, char* argv[]) {
   int ret = 0;
   struct table table;
   char* csvtext = NULL;
+
   if (argc != 2) {
     fprintf(stderr, "Wrong number of args\n");
     ret = EINVAL;
     goto end;
   }
+
   if ((fd = fopen(argv[1], "r")) == NULL) {
     fprintf(stderr, "Failed to open file %s\n", argv[1]);
     ret = ENOENT;
     goto end;
   }
+
   fseek(fd, 0, SEEK_END);
   size_t sz = ftell(fd);
   rewind(fd);
   csvtext = (char*)malloc((sz + 1) * sizeof(char));
+
   if (csvtext == NULL) {
     fprintf(stderr, "Failed to alloc mem for file content\n");
     ret = ENOMEM;
     goto end;
   }
+
   if (fread(csvtext, sizeof(char), sz, fd) != sz) {
     fprintf(stderr, "Failed to read file contents\n");
     ret = EIO;
